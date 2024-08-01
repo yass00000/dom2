95491150f14aac214252fd944cdc3e6bc8e0b35b
yass00000 <yassinelachtar96@gmail.com>
2024-08-01 10:38:25 +0100

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded',function(){})
+//     console.log('dom loaded and parsed');
+    var ChangeColorBtn=document.getElementById('change-color-btn')
+    var ColorBox=document.getElementById('color-box')
+
+
+
+
+function changecolortored(){
+    ColorBox.style.backgroundColor="red"
+    // var letters = '0123456789ABCDEF'
+    // let color ='#'
+    // for (let i = 0; i < 6; i++) {
+    //     color+= letters[Math.floor(Math.random()*16)]
+    // }
+    // return color
+}
